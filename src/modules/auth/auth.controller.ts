d2f1bb6c0dd9f231861479507b843b2475864ea6
yonguk <lyw3059@gmail.com>
2024-11-14 01:10:25 +0900

    fix: bounds from query dto type convert
    쿼리로 받은 dto 타입 변경으로 오류 해결

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,7 +5,6 @@ import { Request, Response } from 'express';
 import { FRONTEND_URL } from 'src/configs/env.config';
 import { GoogleUserProps } from 'src/common/types/Props';
 import { UserService } from 'src/modules/user/user.service';
-import { UserException, UserExceptionCode } from 'src/common/exceptions/Exceptions';
 
 @Controller('auth')
 export class AuthController {
--- src/modules/map/dto/BoundsQuery.dto.ts
+++ src/modules/map/dto/BoundsQuery.dto.ts
@@ -1,15 +1,20 @@
-import { IsNumber } from "class-validator";
+import { IsNumber } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class BoundsQueryDto {
     @IsNumber()
+    @Transform(({ value }) => parseFloat(value))
     neLat: number;
 
     @IsNumber()
+    @Transform(({ value }) => parseFloat(value))
     neLng: number;
 
     @IsNumber()
+    @Transform(({ value }) => parseFloat(value))
     swLat: number;
 
     @IsNumber()
+    @Transform(({ value }) => parseFloat(value))
     swLng: number;
 }
